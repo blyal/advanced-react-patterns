220edfdc72a95faa3133aa1240c04b0c514a568b
blyal <blyal@protonmail.com>
2023-04-02 21:07:50 +0200

    exercise 4: prop collections & prop getters

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -4,23 +4,64 @@
 import * as React from 'react'
 import {Switch} from '../switch'
 
+// See extra credit video for explanation
+function callAll(...fns) {
+  return (...args) => {
+    fns.forEach(fn => {
+      fn && fn(...args)
+    })
+  }
+}
+
 function useToggle() {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
 
   // 🐨 Add a property called `togglerProps`. It should be an object that has
   // `aria-pressed` and `onClick` properties.
-  // 💰 {'aria-pressed': on, onClick: toggle}
-  return {on, toggle}
+  const togglerProps = {'aria-pressed': on, onClick: toggle}
+
+  // Extra Credit function: prop getters
+  const getTogglerProps = ({onClick, ...props}) => {
+    return {
+      'aria-pressed': on,
+      onClick: callAll(onClick, toggle),
+      ...props,
+    }
+  }
+
+  return {on, toggle, togglerProps, getTogglerProps}
 }
 
+// so now we made it so that both the Switch and the button both have
+// the onClick (and any other togglerProps contents too)
+// function App() {
+//   const {on, togglerProps} = useToggle()
+//   return (
+//     <div>
+//       <Switch on={on} {...togglerProps} />
+//       <hr />
+//       <button aria-label="custom-button" {...togglerProps}>
+//         {on ? 'on' : 'off'}
+//       </button>
+//     </div>
+//   )
+// }
+
+// Extra Credit: Prop Getters
 function App() {
-  const {on, togglerProps} = useToggle()
+  const {on, getTogglerProps} = useToggle()
   return (
     <div>
-      <Switch on={on} {...togglerProps} />
+      <Switch {...getTogglerProps({on})} />
       <hr />
-      <button aria-label="custom-button" {...togglerProps}>
+      <button
+        {...getTogglerProps({
+          'aria-label': 'custom-button',
+          onClick: () => console.info('onButtonClick'),
+          id: 'custom-button-id',
+        })}
+      >
         {on ? 'on' : 'off'}
       </button>
     </div>
